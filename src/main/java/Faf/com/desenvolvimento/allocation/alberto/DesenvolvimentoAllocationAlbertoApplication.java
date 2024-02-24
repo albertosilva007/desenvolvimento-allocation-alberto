b9a55eab6d07ea6cac60b50fe4f97dc16ca89247
José Alberto Claudino Silva <pr.josealbertoibc@gmail.com>
2024-02-24 16:49:35 -0300

    add project

---
+++ src/main/java/Faf/com/desenvolvimento/allocation/alberto/DesenvolvimentoAllocationAlbertoApplication.java
@@ -0,0 +1,13 @@
+package Faf.com.desenvolvimento.allocation.alberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesenvolvimentoAllocationAlbertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesenvolvimentoAllocationAlbertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Faf/com/desenvolvimento/allocation/alberto/DesenvolvimentoAllocationAlbertoApplicationTests.java
@@ -0,0 +1,13 @@
+package Faf.com.desenvolvimento.allocation.alberto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesenvolvimentoAllocationAlbertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
